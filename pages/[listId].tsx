5b96ee914bdc731b88aee23273c529c3d899042a
heavensbee001 <heavensbee001@gmail.com>
2022-01-24 15:05:26 +0100

    Update list pages to be dynamic

---
+++ pages/[listId].tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import { useRouter } from 'next/router'
+import { allowedNodeEnvironmentFlags } from 'process'
+import { syncBuiltinESMExports } from 'module'
+
+export default function Jobs() {
+	const router = useRouter()
+
+	return (
+		<Layout>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section>
+				<p>Dynamic page</p>
+			</section>
+		</Layout>
+	)
+}
+
+export async function getStaticPaths() {
+	return {
+		paths: [{ params: { listId: 'hackathons' } }, { params: { listId: 'jobs' } }],
+		fallback: false,
+	}
+}
+
+export async function getStaticProps() {
+	return { props: {} }
+}
