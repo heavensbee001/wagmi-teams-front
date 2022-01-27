e14d3a5f3b801254c6d7e13c8d832ac7bbd64ebe
heavensbee001 <heavensbee001@gmail.com>
2022-01-27 14:03:48 +0100

    Create home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,32 @@ export default function Home() {
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
+
 			<section>
+				<h2 className="bg-font text-5xl leading-snug tracking-wider">
+					TEAM UP
+					<br />
+					WITH AMAZING
+					<br />
+					PEOPLE
+				</h2>
+			</section>
+			<section className="bv-font text-3xl text-white">
 				<p>
-					<Link href="/hackathons">HACKATHONS</Link>
+					<Link href="/hackathons" passHref>
+						<div className="group">
+							<span className="group-hover:underline group-hover:italic group-hover:cursor-pointer">hackathons</span>
+							<span className="pl-6 group-hover:cursor-pointer group-hover:pl-8">→</span>
+						</div>
+					</Link>
 				</p>
 				<p>
-					<Link href="/jobs">JOBS</Link>
+					<Link href="/jobs" passHref>
+						<div className="group">
+							<span className="group-hover:underline group-hover:italic group-hover:cursor-pointer">jobs</span>
+							<span className="pl-6 hover:cursor-pointer group-hover:pl-8">→</span>
+						</div>
+					</Link>
 				</p>
 			</section>
 		</Layout>
