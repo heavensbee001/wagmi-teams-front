920f174b3a1aa70f694cc3034a4cd1669f5faa37
heavensbee001 <heavensbee001@gmail.com>
2022-02-17 20:19:40 +0100

    Add submission handler

--- components/createPositionForm.tsx
+++ components/createPositionForm.tsx
@@ -1,10 +1,18 @@
-import { FC, Fragment, useState, useContext } from 'react'
+import { FC, Fragment, useState, useContext, useReducer } from 'react'
 import { StoreContext } from '../pages/_app'
 import ConnectWalletButton from './connectWalletButton'
+
+const formReducer = (state, event) => {
+	return {
+		...state,
+		[event.target.name]: event.target.value,
+	}
+}
 
 const CreatePositionForm: FC<any> = () => {
 	const storeContext = useContext(StoreContext)
 	const [createFormActive, setCreateFormActive] = useState(false)
+	const [formData, setFormData] = useReducer(formReducer, {})
 
 	const handleClickAddPosition = () => {
 		setCreateFormActive(!createFormActive)
@@ -12,6 +20,11 @@ const CreatePositionForm: FC<any> = () => {
 
 	const handleInputWheel = e => {
 		e.target.blur()
+	}
+
+	const handleSubmit = e => {
+		console.log(formData)
+		e.preventDefault()
 	}
 
 	return (
@@ -23,16 +36,16 @@ const CreatePositionForm: FC<any> = () => {
 				</button>
 				<div className="overflow-y-scroll top-0 left-0 w-full h-full p-4 pb-8">
 					{storeContext.state.currentAccount ? (
-						<form>
-							<input id="companyName" type="text" placeholder="Company Name*" required className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
-							<input id="positionTitle" type="text" placeholder="Position Title*" required className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
-							<textarea id="description" placeholder="Description*" required maxLength={280} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
-							<input id="positionUrl" type="text" placeholder="Position url" required className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
-							<input id="contact" type="text" placeholder="contact" required className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
+						<form onSubmit={handleSubmit}>
+							<input name="companyName" type="text" placeholder="Company Name*" required onChange={setFormData} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
+							<input name="positionTitle" type="text" placeholder="Position Title*" required onChange={setFormData} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
+							<textarea name="description" placeholder="Description*" required maxLength={280} onChange={setFormData} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
+							<input name="positionUrl" type="text" placeholder="Position url" required onChange={setFormData} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
+							<input name="contact" type="text" placeholder="contact" required onChange={setFormData} className="border-b-2 border-b-black w-full mb-4 py-2 placeholder-black" />
 							<label htmlFor="tipCreator" className="mr-2">
 								Tip creator
 							</label>
-							<input id="tipCreator" type="number" placeholder="0" className="border-b-2 border-b-black mb-4 py-2 pr-10 placeholder-black text-right appearance-none" onWheel={handleInputWheel} />
+							<input name="tipCreator" type="number" placeholder="0" onChange={setFormData} className="border-b-2 border-b-black mb-4 py-2 pr-10 placeholder-black text-right appearance-none" onWheel={handleInputWheel} />
 							<img src="/polygon-matic-logo.svg" width={30} alt="MATIC" className="inline-block -ml-8 mb-1" />
 							<span className="ml-2">MATIC</span>
 							<div className="w-full text-right">
