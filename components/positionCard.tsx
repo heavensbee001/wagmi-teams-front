cf10aa87fd518a2e64b157889a8b13ac68dde076
heavensbee001 <heavensbee001@gmail.com>
2022-03-26 17:04:41 +0100

    Add LetteredAvatar to the position card

--- components/positionCard.tsx
+++ components/positionCard.tsx
@@ -1,11 +1,46 @@
-import { FC } from 'react'
+import { ComponentType, FC } from 'react'
+import dynamic from 'next/dynamic'
 
-const PositionCard: FC<any> = ({ position }) => {
+type LetteredAvatarProps = {
+	name: string
+	radius: number
+}
+
+const LetteredAvatar: ComponentType<LetteredAvatarProps> = dynamic(
+	import('react-lettered-avatar').then(module => {
+		console.log(module)
+		console.log(module.default)
+		return module.default
+	}),
+	{
+		ssr: false,
+	}
+)
+
+//@TODO: create a global type for position
+type Props = {
+	position: {
+		title: string
+		projectOrCompanyName: string
+		projectOrCompanyImageUrl: string
+		description: string
+		positionOfferUrl: string
+		contact: string
+		createdAt: string
+	}
+}
+
+const PositionCard: FC<Props> = ({ position }) => {
 	return (
-		<li className="relative pt-8 mb-4">
-			<img src="https://via.placeholder.com/150" className="absolute top-0 w-16" alt={position.projectOrCompanyName} />
+		<li className="relative pt-6 mb-4">
+			{LetteredAvatar && (
+				<div className="absolute top-0 w-16">
+					<LetteredAvatar name={position.projectOrCompanyName[0]} radius={0} />
+				</div>
+			)}
+			{/* <img src="https://via.placeholder.com/150" className="absolute top-0 w-16" alt={position.projectOrCompanyName} /> */}
 			<div className="bg-white ml-5 py-2 px-4">
-				<h4 className="ml-10 mb-2">
+				<h4 className="ml-8 mb-2">
 					<span className="bg-font">{position.title}</span>
 					<span className="bv-font"> @ {position.projectOrCompanyName}</span>
 				</h4>
